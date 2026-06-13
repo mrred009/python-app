a82c81e645fbeb37b0c261eb6422793cbe5099d4
Darshan <darshanpvt@gmail.com>
2026-06-13 11:48:17 -0400

    Convert to Flask app

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,8 @@ WORKDIR /app
 
 COPY . .
 
+RUN pip install -r requirements.txt
+
+EXPOSE 5000
+
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello from Jenkins CI/CD")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Jenkins CI/CD"
+
+app.run(host="0.0.0.0", port=5000)
